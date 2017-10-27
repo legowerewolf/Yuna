8ee679b7c133508694de95b66211c6357a17853c
legowerewolf <legowerewolf@gmail.com>
2017-10-26 19:34:01 -0700

    Learned to detect keywords and act on the Mute keyword!

--- yuna.go
+++ yuna.go
@@ -5,9 +5,28 @@ import (
 	"strings"
 )
 
+type person struct {
+	discordID string
+	names     []string
+	prefname  string
+}
+
+type data struct {
+	people []person
+}
+
 func main() {
-	command := "Yuna, mute Josh."
-	fmt.Println(sanitize(command))
+	object := data{
+		people: []person{
+			person{
+				prefname:  "Josh",
+				names:     []string{"Josh", "Rippey", "Eragon"},
+				discordID: "210220334756921345"},
+			person{
+				prefname:  "Rachel",
+				discordID: "226895271022428160"}}}
+	command := "Yuna, mute Eragon."
+	interpret(command, object)
 }
 
 func sanitize(s string) []string {
@@ -18,4 +37,29 @@ func sanitize(s string) []string {
 
 	words := strings.Split(s, " ")
 	return words
+}
+
+func interpret(command string, data data) {
+	s := sanitize(command)
+	for i, word := range s {
+		word = strings.ToLower(word)
+		switch word {
+		case "mute":
+			fmt.Println("keyword mute detected")
+			target := s[i+1]
+			discordID := ""
+			for _, person := range data.people {
+				for _, name := range person.names {
+					if name == target {
+						fmt.Println("Target match: " + person.prefname)
+						discordID = person.discordID
+					}
+				}
+			}
+			fmt.Println("Muting ID " + string(discordID))
+			return
+		default:
+			fmt.Println("")
+		}
+	}
 }
