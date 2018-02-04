7fa5f0ba242339fe5737129808b1ca0b5eff18d7
legowerewolf <legowerewolf@gmail.com>
2018-02-03 17:19:31 -0800

    Updated build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 @ECHO off
 
+ECHO [Docker] Starting Docker Host VM
+docker-machine start default
+
 ECHO [Docker] Setting variables...
 @FOR /f "tokens=*" %%i IN ('docker-machine env default') DO @%%i
 
